e43cd2973ed8f48f583622737b1cc7948f0b0f85
edo9k <edox86@gmail.com>
2019-06-24 18:01:24 -0300

    All changes up to class 10
    The last thing I was doing here was the creation of a user in the
    MongoDB Atlas test cluster. About 11:45 into the 11th class.

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 /** @format */
 
 const express = require('express')
+const connectDB = require('./config/db')
 
+// initialize express
 const app = express()
 
+// connect do mongodb
+connectDB()
+
+// Init bodyparser
+app.use(express.json({ extended: false }))
+
+// test route
 app.get('/', (req, res) => res.send('API Running'))
+
+// define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/posts', require('./routes/api/posts'))
 
 const PORT = process.env.PORT || 5000
 
