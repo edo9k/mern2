e2146c34645ddad53540ee1ccadc3d99115f8112
edo9k <edox86@gmail.com>
2019-11-01 18:33:24 -0300

    Register Form and useState (class 34)

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import React, { Fragment } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar.js'
 import Landing from './components/layout/Landing.js'
-
+import Register from './components/auth/Register.js'
+import Login from './components/auth/Login.js'
 import './App.css'
 
 const App = () => (
@@ -12,6 +13,12 @@ const App = () => (
     <Fragment>
       <Navbar />
       <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
     </Fragment>
   </Router>
 )
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,6 +1,7 @@
 /** @format */
 
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
@@ -12,13 +13,13 @@ const Landing = () => {
           <p className="lead">Create a developer profile/portfolio, share posts and get help from other developers</p>
 
           <div className="buttons">
-            <a href="register.html" className="btn btn-primary">
+            <Link to="/register" className="btn btn-primary">
               Sign Up
-            </a>
+            </Link>
 
-            <a href="login.html" className="btn btn-light">
+            <Link to="/login" className="btn btn-light">
               Login
-            </a>
+            </Link>
           </div>
         </div>
       </div>
